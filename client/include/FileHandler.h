cb8f73fcc8800c8122f6b507c4e31e71810e0316
Marcin Hrycaj <marcin.hrycaj@student.put.poznan.pl>
2017-02-20 00:50:48 +0100

    Changed forward declaration of PeerFile in FileHandler.h to header include

--- client/include/FileHandler.h
+++ client/include/FileHandler.h
@@ -7,10 +7,10 @@
 #include <memory>
 #include <unordered_set>
 #include <map>
+#include <PeerFile.h>
 #include "File.h"
 
 class File;
-class PeerFile;
 
 class FileHandler : public Observer {
     public:
