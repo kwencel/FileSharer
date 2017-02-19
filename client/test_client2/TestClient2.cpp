6d14012365ca405b430b8fee3d9846d247ab5dd8
Krzysztof Wencel <kwencel@users.noreply.github.com>
2017-02-19 23:33:00 +0100

    Fixed compilation error.

---
+++ client/test_client2/TestClient2.cpp
@@ -0,0 +1,26 @@
+#include "MainWindow.h"
+#include <QApplication>
+#include <easylogging++.h>
+#include <boost/filesystem/operations.hpp>
+#include <SerializationHelper.h>
+
+INITIALIZE_EASYLOGGINGPP
+
+int main(int argc, char *argv[]) {
+//    File sinewave("example_file.flac");
+//
+//    File kopia("kopia_chunked.flac", sinewave.getRealSize(), sinewave.getHash(), sinewave.getChunksHashes());
+//
+//    std::vector<Chunk *> kopiaChunks = kopia.getChunks();
+//
+//    for (unsigned long i = 0; i < kopia.getChunksAmount(); ++i) {
+//        kopiaChunks[i]->setData(sinewave.readChunkData(i));
+//    }
+
+    std::cout << QT_VERSION_STR;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
